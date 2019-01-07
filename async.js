4deabe57c8d9b5cc52418fa24e98f8818f3dee40
Ragupathi <ragupathi@revmakx.com>
2019-01-07 14:39:14 +0530

    async await program

--- async.js
+++ async.js
@@ -1,6 +1,27 @@
 var baseUrl = 'https://hacker-news.firebaseio.com/v0';
-
-var fetch =  async function(url,filename){
-	console.log(url);
-}
-await fetch(baseUrl,'ragu');
+	async function test(){
+		var hackerNews = async function(url,filename){
+			try{
+				var responseData =  await fetch(url+'/'+filename);
+				return await responseData.json();
+			}catch(error){
+				console.log('error');
+			}
+		}
+		var showstories = await Promise.all([hackerNews(baseUrl,'showstories.json'), hackerNews(baseUrl,'topstories.json')]);
+		try{
+			showstories.forEach(function(value,key){
+				var showstoriesReport = value.splice(0,1);
+				showstoriesReport.forEach(async function(value,key){
+					var reportData = await fetch(baseUrl + '/item/' + value + '.json');
+					var reportResponse = await reportData.json();
+					var title = reportResponse.title;
+					var url = reportResponse.url;
+					$('#content').append('<a  target="_blank" href="' + url + '" >' + title + '</a><br />');		
+				});
+			})
+		}catch(error){
+		console.log('error');
+		}
+	};
+test();
